8298c7276a079e3837f8683ff166661d4cbe7bfc
MarkFergus <github@accounts.markfergus.com>
2024-05-06 23:48:17 +0200

    Modal details added with some temp info for show

--- src/Modal.js
+++ src/Modal.js
@@ -14,12 +14,53 @@ class Modal extends Component {
         return (
             <div className="Modal">
                 <div className="Modal-overlay" onClick={this.toggleModal}></div>
-                <div className="Modal-window">
-                    <div className="Modal-close" onClick={this.toggleModal}>
-                        <IconSquareRoundedX />
-                    </div>
-                    <div className="Modal-content">
-                        <h1>This is the modal window</h1>
+                <div className="Modal-windowbox">
+                    <div className="Modal-window">
+                        <div className="Modal-close" onClick={this.toggleModal}>
+                            <IconSquareRoundedX />
+                        </div>
+                        <div className="Modal-content">
+                            <h1 className="Modal-title">
+                                {this.props.monkey.name}
+                            </h1>
+                            <div className="Modal-img">
+                                <img
+                                    src={this.props.monkey.img}
+                                    alt={this.props.monkey.name}
+                                ></img>
+                            </div>
+                            <div className="Modal-details">
+                                <h3>
+                                    Troop:{" "}
+                                    <span>{this.props.monkey.troop}</span>
+                                </h3>
+                                <h3>
+                                    Sex: <span>{this.props.monkey.sex}</span>
+                                </h3>
+                                <h3>
+                                    Born: <span>{this.props.monkey.year}</span>
+                                </h3>
+                                <h3>
+                                    Chip: <span>42888</span>
+                                </h3>
+                                <h3>
+                                    Bio:{" "}
+                                    <span>
+                                        Arrived as orphan together with another
+                                        orphan. Mother was killed by a
+                                        velociraptor. Really likes toast.
+                                        Fostered by Big Mama.
+                                    </span>
+                                </h3>
+                                <h3>
+                                    Description:{" "}
+                                    <span>
+                                        Golden coat, pacthy silhouettes around
+                                        eyes, slight notch on left ear.
+                                    </span>
+                                </h3>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
