c61a1777419718f3e98c1db2502e7d4f16f93aa9
MarkFergus <github@accounts.markfergus.com>
2024-05-07 14:19:35 +0200

    modified monkey array - added chip values & more records

--- src/monkeysArr.js
+++ src/monkeysArr.js
@@ -4,6 +4,7 @@ const monkeysArr = [
         troop: "Koko",
         sex: "male",
         year: 2023,
+        chip: 41192,
         img: "https://i.ibb.co/s1zM2FL/yoda-koko-feb2024-2.jpg",
     },
     {
@@ -11,6 +12,7 @@ const monkeysArr = [
         troop: "Gismo",
         sex: "male",
         year: 2023,
+        chip: 41197,
         img: "https://i.ibb.co/k3q8XXr/timmy-gismo-mar2024.jpg",
     },
     {
@@ -18,6 +20,7 @@ const monkeysArr = [
         troop: "Koko",
         sex: "male",
         year: 2023,
+        chip: 26665,
         img: "https://i.ibb.co/hX8Zs3c/riccardo-koko-mar2024.jpg",
     },
     {
@@ -25,6 +28,7 @@ const monkeysArr = [
         troop: "Koko",
         sex: "male",
         year: 2023,
+        chip: 26609,
         img: "https://i.ibb.co/xD4brFX/atlas-koko-feb2024.jpg",
     },
     {
@@ -32,6 +36,7 @@ const monkeysArr = [
         troop: "Skunkey",
         sex: "female",
         year: 2023,
+        chip: 41179,
         img: "https://i.ibb.co/jJCSQdx/tk-skunkey-mar2024.jpg",
     },
     {
@@ -39,6 +44,7 @@ const monkeysArr = [
         troop: "Koko",
         sex: "male",
         year: 2023,
+        chip: 41181,
         img: "https://i.ibb.co/Xy8Pn6y/samber-koko-mar2024.jpg",
     },
     {
@@ -46,6 +52,7 @@ const monkeysArr = [
         troop: "H&B",
         sex: "male",
         year: 2023,
+        chip: 41192,
         img: "https://i.ibb.co/d26Jdcb/paulie-hb-mar2024-2.jpg",
     },
     {
@@ -53,6 +60,7 @@ const monkeysArr = [
         troop: "Goliath",
         sex: "male",
         year: 2023,
+        chip: 41186,
         img: "https://i.ibb.co/vzbDSLK/nugget-goliath-mar2024-2.jpg",
     },
     {
@@ -60,13 +68,31 @@ const monkeysArr = [
         troop: "Goliath",
         sex: "female",
         year: 2023,
+        chip: 41220,
         img: "https://i.ibb.co/m0tjhzm/wiz-goliath-apr2024.jpg",
+    },
+    {
+        name: "Mary Sunshine",
+        troop: "Goliath",
+        sex: "female",
+        year: 2023,
+        chip: 0,
+        img: "https://i.ibb.co/1bkzt5L/marysunshine-goliath-feb2024.jpg",
+    },
+    {
+        name: "Lochlan",
+        troop: "Royal",
+        sex: "female",
+        year: 2023,
+        chip: 0,
+        img: "https://i.ibb.co/jLS22sC/lochlan-royal-apr2024.jpg",
     },
     {
         name: "Kathleen",
         troop: "H&B",
         sex: "female",
         year: 2023,
+        chip: 41183,
         img: "https://i.ibb.co/XZXhgdQ/kathleen-hb-mar2024-2.jpg",
     },
     {
@@ -74,6 +100,7 @@ const monkeysArr = [
         troop: "Global",
         sex: "female",
         year: 2023,
+        chip: 41196,
         img: "https://i.ibb.co/7jcS8Fp/june-global-mar2024-2.jpg",
     },
     {
@@ -81,6 +108,7 @@ const monkeysArr = [
         troop: "D&D",
         sex: "male",
         year: 2023,
+        chip: 41222,
         img: "https://i.ibb.co/PrqtB6n/harold-dd-apr2024.jpg",
     },
     {
@@ -88,6 +116,7 @@ const monkeysArr = [
         troop: "Engeltjie",
         sex: "female",
         year: 2023,
+        chip: 41221,
         img: "https://i.ibb.co/hyRQNQ2/fritzi-engeltjie-mar204-2.jpg",
     },
     {
@@ -95,6 +124,7 @@ const monkeysArr = [
         troop: "Skunkey",
         sex: "female",
         year: 2023,
+        chip: 41180,
         img: "https://i.ibb.co/C130wG9/elora-skunkey-mar2024-2.jpg",
     },
     {
@@ -102,6 +132,7 @@ const monkeysArr = [
         troop: "James",
         sex: "male",
         year: 2023,
+        chip: 41200,
         img: "https://i.ibb.co/JCCYGjL/darby-james-feb2024-2.jpg",
     },
     {
@@ -109,6 +140,7 @@ const monkeysArr = [
         troop: "Skunkey",
         sex: "female",
         year: 2023,
+        chip: 41185,
         img: "https://i.ibb.co/JFD3PyW/anita-skunkey-mar2024-2.jpg",
     },
     {
@@ -116,6 +148,7 @@ const monkeysArr = [
         troop: "Goliath",
         sex: "male",
         year: 2023,
+        chip: 41184,
         img: "https://i.ibb.co/Sn78j7j/rowan-goliath-mar2024.jpg",
     },
     {
@@ -123,6 +156,7 @@ const monkeysArr = [
         troop: "H&B",
         sex: "female",
         year: 2023,
+        chip: 41182,
         img: "https://i.ibb.co/8s9GpbG/tufyt-hb-feb2024.jpg",
     },
     {
@@ -130,6 +164,7 @@ const monkeysArr = [
         troop: "H&B",
         sex: "female",
         year: 2023,
+        chip: 41188,
         img: "https://i.ibb.co/hKkLRhs/wanda-hb-mar2024.jpg",
     },
     {
@@ -137,6 +172,7 @@ const monkeysArr = [
         troop: "H&B",
         sex: "female",
         year: 2023,
+        chip: 41187,
         img: "https://i.ibb.co/dQn22jN/bluebell-hb-mar2024.jpg",
     },
     {
@@ -144,6 +180,7 @@ const monkeysArr = [
         troop: "H&B",
         sex: "male",
         year: 2023,
+        chip: 41193,
         img: "https://i.ibb.co/rQ9mJN7/mikey-hb-feb2024.jpg",
     },
     {
@@ -151,6 +188,7 @@ const monkeysArr = [
         troop: "D&D",
         sex: "female",
         year: 2023,
+        chip: 41211,
         img: "https://i.ibb.co/ZBw3DfT/mia-dd-mar2024.jpg",
     },
     {
@@ -158,6 +196,7 @@ const monkeysArr = [
         troop: "D&D",
         sex: "female",
         year: 2023,
+        chip: 41217,
         img: "https://i.ibb.co/jw7bC76/ildiko-dd-apr2024.jpg",
     },
     {
@@ -165,6 +204,7 @@ const monkeysArr = [
         troop: "Engeltjie",
         sex: "male",
         year: 2023,
+        chip: 41195,
         img: "https://i.ibb.co/Q90b47c/remy-engeltjie-may2024.jpg",
     },
     {
@@ -172,6 +212,7 @@ const monkeysArr = [
         troop: "Engeltjie",
         sex: "female",
         year: 2023,
+        chip: 41225,
         img: "https://i.ibb.co/chtSfPC/duimpie-engeltjie-apr2024-temp.jpg",
     },
     {
@@ -179,6 +220,7 @@ const monkeysArr = [
         troop: "Global",
         sex: "female",
         year: 2023,
+        chip: 41178,
         img: "https://i.ibb.co/J35734k/lyyti-global-feb2024.jpg",
     },
     {
@@ -186,6 +228,7 @@ const monkeysArr = [
         troop: "Bandits",
         sex: "male",
         year: 2023,
+        chip: 41190,
         img: "https://i.ibb.co/RzMJ63S/bugaloo-bandits-mar2024.jpg",
     },
     {
@@ -193,6 +236,7 @@ const monkeysArr = [
         troop: "SAAV",
         sex: "female",
         year: 2023,
+        chip: 41218,
         img: "https://i.ibb.co/t4B6Brv/trixi-saav-apr2024.jpg",
     },
     {
@@ -200,6 +244,7 @@ const monkeysArr = [
         troop: "SAAV",
         sex: "male",
         year: 2023,
+        chip: 41219,
         img: "https://i.ibb.co/rtKmkfc/harris-saav-apr2024.jpg",
     },
 ];
